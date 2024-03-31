a7a4b5ed3e61989c60c8ed65ee9b88a017a47189
mindflaretech <wetrack.pk@gamil.com>
2024-04-01 02:51:32 +0500

    urlChanges

--- helpers/apollo.js
+++ helpers/apollo.js
@@ -12,7 +12,7 @@ function createApolloClient() {
   return new ApolloClient({
     ssrMode: typeof window === "undefined",
     link: new HttpLink({
-      uri: "https://bigdeal-api-git-main-pixelstrapthemes.vercel.app/", // Server URL (must be absolute)
+      uri: "https://api-gamma-olive.vercel.app", // Server URL (must be absolute)
       credentials: "same-origin", // Additional fetch() options like `credentials` or `headers`
       fetch,
     }),
